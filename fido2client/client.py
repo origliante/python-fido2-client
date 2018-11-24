cef92a3ebc760c43084696a92fe6a78c1710abbd
origliante <6919114+origliante@users.noreply.github.com>
2018-11-24 20:33:34 +0100

    support to pass an existing session or keep using the newly created session; support to append custom headers and data

--- fido2client/client.py
+++ fido2client/client.py
@@ -21,7 +21,9 @@ class Fido2HttpClient(object):
     is_authenticated = False
     verbose = False
 
-    def authenticate_to(self, server, begin_endpoint, complete_endpoint, session=None):
+    def authenticate_to(self,
+            server, begin_endpoint, complete_endpoint,
+            session=None, append_to_data=None, append_to_headers=None):
         self.server = server
         self.init_dev()
         begin_url = urllib.parse.urljoin(server, begin_endpoint)
@@ -34,8 +36,11 @@ class Fido2HttpClient(object):
             if not self.session:
                 self.session = requests.session()
 
-            self.begin(begin_url)
-            self.complete(complete_url)
+            self.begin(begin_url,
+                append_to_headers=append_to_headers)
+            self.complete(complete_url,
+                append_to_data=append_to_data,
+                append_to_headers=append_to_headers)
         return self.is_authenticated
 
     def log(self, *args):
@@ -59,13 +64,20 @@ class Fido2HttpClient(object):
         if not self.dev:
             self.say_no_device_found()            
 
-    def begin(self, begin_url):
+    def begin(self, begin_url, append_to_headers=None):
+        headers = {}
+        if append_to_headers:
+            for k in append_to_headers:
+                headers[k] = append_to_headers[k]
+
         r = self.session.post(begin_url,
-            verify=self.ssl_verify)
+            verify=self.ssl_verify,
+            headers=headers)
+
         self.begin_data = cbor.loads(r.content)
         self.log('BEGIN RESPONSE: ', self.begin_data)
 
-    def complete(self, complete_url):
+    def complete(self, complete_url, append_to_data=None, append_to_headers=None):
         fido2_client = Fido2Client(self.dev, self.server)
 
         pubkey = self.begin_data['publicKey']
@@ -84,28 +96,42 @@ class Fido2HttpClient(object):
             allow_list)
             #TODO
             #pin=getpass('Please enter PIN:'))
+
         assertion = assertions[0]
         self.log('ASSERTION: ', assertion)
         self.log('CLIENT DATA: ', client_data)
 
-        body = cbor.dumps({
+        data = {
             'credentialId': assertion.credential['id'],
             'authenticatorData': assertion.auth_data,
             'clientDataJSON': client_data,
             'signature': assertion.signature,
-        })
+            'user_data': append_to_data,
+        }
+        body = cbor.dumps(data)
+        headers = {'content-type': 'application/cbor'}
+        if append_to_headers:
+            for k in append_to_headers:
+                headers[k] = append_to_headers[k]
 
         r = self.session.post(complete_url,
             verify=self.ssl_verify,
-            headers={'Content-Type': 'application/cbor'},
+            headers=headers,
             data=body
         )
+        self._last_response = r
+
         data = cbor.loads(r.content)
         self.log('COMPLETE RESPONSE: ', data)
         if 'status' in data and data['status'] == 'OK':
-            self.say_authenticated()
             self.is_authenticated = True
         else:
             self.is_authenticated = False
-            self.say_not_authenticated(data)
+
+        if self.is_authenticated: self.say_authenticated()
+        else: self.say_not_authenticated(data)
+        return self.is_authenticated
+
+    def get_last_response(self):
+        return self._last_response
 

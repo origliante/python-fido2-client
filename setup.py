0e0227b628270052eda383a76e2cc76061808a06
origliante <6919114+origliante@users.noreply.github.com>
2018-11-24 16:20:57 +0100

    LICENSE updated; added setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fido2client",
+    version="0.9.0",
+    author="origliante",
+    author_email="please.visit.github@page.nowhere",
+    description="WebAuthn API FIDO2 client",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/origliante/python-fido2-client",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    install_requires=[
+        'fido2',
+        'cbor2',
+        'requests',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License", 
+        "Operating System :: OS Independent",
+        'Topic :: Internet',
+        'Topic :: Security :: Cryptography',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
+
